83d13263ef04fb9b08341cd8a3e2bb4a90f0a93a
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-02-20 17:47:59 +0300

    add subscription_visit model

--- src/sport/models.py
+++ src/sport/models.py
@@ -28,5 +28,25 @@ class SubscriptionModel(models.Model):
 
     quantity = models.SmallIntegerField(
         verbose_name='Количество занятий',
-        
+
+    )
+
+
+class SubscriptionVisits(models.Model):
+    user = models.ForeignKey(
+        user,
+        verbose_name='Пользователь',
+        related_name='user_subscription_visits',
+        on_delete=models.PROTECT
+    )
+    subscription = models.ForeignKey(
+        'sport.SubscriptionModel',
+        verbose_name='Абонемент',
+        related_name='subscription_subscription_visits',
+        on_delete=models.PROTECT
+    )
+    visit_date = models.DateTimeField(
+        verbose_name='Дата посещения',
+        auto_now_add=True,
+
     )
